563ac93c966cd4dd6b432ff4aedfec905a8a6e46
Amit Vaidya <amitvaidhya@gmail.com>
2023-12-20 21:16:01 +0000

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Amit Vaidya on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
